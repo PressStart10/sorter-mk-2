808f74f9ee54877ef6923559550d49a4f1bcb5eb
PressStart10 <angela_litovchick@hotmail.com>
2023-09-10 19:09:29 -0400

    OPTIMIZE PRIME

--- sorter mk 2/a_sort.cpp
+++ sorter mk 2/a_sort.cpp
@@ -5,62 +5,18 @@
 #include <Windows.h>
 #include <cstdlib>
 #include <time.h>
+#include "sort.hpp"
 using namespace std;
 const int leng = 100;
 int array_to_sort[leng];
-void swap(int* ar, int index1, int index2) {
-    int indexs1 = ar[index1];
-    int indexs2 = ar[index2];
-    ar[index1] = indexs2;
-    ar[index2] = indexs1;
-}
-void shuffle(int* shufflearray,int n) {
-    srand(time(NULL));
-    for (int i = 0; i < n; i++) {
-        shufflearray[i] = i+1;
-    }
-    //is this unnessisary? yes. do I care? no. will I change it? probably not.
-    for (int i = 0; i < n; i++) {
-        swap(shufflearray, i, rand() % n);
-    }
-}
-void printarray(int *ar, int len) {
-    for (int i = 0; i < len; i++) {
-        cout << ar[i] << " : ";
-    }
-    cout << endl;
-}
-void sort(int* ar, int len) {
-    int correct = 0;
-    while (correct != len) {
-        for (int i = 0; i < len; i++) {
-            if ((ar[i] > ar[i + 1])) {
-                swap(ar, i, i + 1);
-            }
-        }
-        for (correct = 0; correct < len; correct++) {
-            if (ar[correct] > ar[correct + 1]) {
-                break;
-            }
-        }
-    }
-}
-bool check(int* ar, int len) {
-    int checks = 0;
-    for (checks; checks < len-1; checks++) {
-        if (ar[checks] > ar[checks + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
 int main()
 {
-    shuffle(array_to_sort, leng);
-    printarray(array_to_sort, (sizeof(array_to_sort) / sizeof(*array_to_sort)));
-    sort(array_to_sort, leng - 1);
-    printarray(array_to_sort, leng);
-    if (check(array_to_sort, leng) == true)
+    base_sort sorts;
+    sorts.shuffle(array_to_sort, leng);
+    sorts.printarray(array_to_sort, (sizeof(array_to_sort) / sizeof(*array_to_sort)));
+    sorts.sort(array_to_sort, leng - 1);
+    sorts.printarray(array_to_sort, leng);
+    if (sorts.check(array_to_sort, leng) == true)
         cout << "sort verified!\n\a";
     else
         cout << "sort malfunction :_(\n\a";
@@ -76,4 +32,3 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
