fb11bd914d6911773d56e64c85ec0764239c10df
PressStart10 <angela_litovchick@hotmail.com>
2023-09-11 21:32:13 -0400

    user input + vector sorts.

--- sorter mk 2/a_sort.cpp
+++ sorter mk 2/a_sort.cpp
@@ -5,21 +5,84 @@
 #include <Windows.h>
 #include <cstdlib>
 #include <time.h>
-#include "sort.hpp"
+#include <vector>
+#include "array_sort.hpp"
+#include "vector_sort.hpp"
 using namespace std;
 const int leng = 100;
 int array_to_sort[leng];
 int main()
 {
     base_sort sorts;
-    sorts.shuffle(array_to_sort, leng);
+    vector_base_sort vsorts;
+    int a = 0;
+    /*sorts.shuffle(array_to_sort, leng);
     sorts.printarray(array_to_sort, (sizeof(array_to_sort) / sizeof(*array_to_sort)));
     sorts.sort(array_to_sort, leng - 1);
     sorts.printarray(array_to_sort, leng);
     if (sorts.check(array_to_sort, leng) == true)
         cout << "sort verified!\n\a";
     else
-        cout << "sort malfunction :_(\n\a";
+        cout << "sort malfunction :_(\n\a";*/
+    cout << "set length of array 1-200.[type]";
+    cin >> a;
+    if (!cin.good()) {
+        cerr << "I have saved your RAM. input a number next time\n";
+        return 0;
+    }
+    while (true) {
+        if ((a < 1) || (a > 200)) {
+            cerr << "please enter a valid number.[type]";
+            cin >> a;
+            continue;
+        }
+        break;
+    }
+    vector<int> vec(a);
+    vsorts.shuffle(vec, vec.size());
+    if (vsorts.check(vec, vec.size()) == true) {
+        cerr << "shuffle not random\n";
+        return 011;
+    }
+    else {
+        cout << "suffle randomized (trust me bro)\nwould you like to see the shuffled array?[y/n]";
+        string c;
+        cin >> c;
+        while (true) {
+            if (c == "y") {
+                vsorts.printarray(vec, vec.size());
+                break;
+            }
+            else if ((c != "y") && (c != "n")) {
+                cerr << "please enter an option.[y/n]";
+                cin >> c;
+                continue;
+            }
+            break;
+        }
+    }
+    vsorts.sort(vec, vec.size()-1);
+    if (vsorts.check(vec, vec.size()) == true) {
+        cout << "sorted!\nwould you like to see the sorted array?[y/n]";
+        string d;
+        cin >> d;
+        while (true) {
+            if (d == "y") {
+                vsorts.printarray(vec, vec.size());
+                break;
+            }
+            else if ((d != "y") && (d != "n")) {
+                cerr << "please enter an option.[y/n]";
+                cin >> d;
+                continue;
+            }
+            break;
+        }
+    }
+    else {
+        cout << "dude im sorry, it didn't work\n";
+            return 0;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- sorter mk 2/vector_sort.hpp
+++ sorter mk 2/vector_sort.hpp
@@ -1,14 +1,15 @@
-#ifndef SORT_HPP
-#define SORT_HPP
-class base_sort {
+
+#ifndef VECTOR_SORT_HPP
+#define VECTOR_SORT_HPP
+class vector_base_sort {
 public:
-    void swap(int* ar, int index1, int index2) {
+    void swap(vector<int>& ar, int index1, int index2) {
         int indexs1 = ar[index1];
         int indexs2 = ar[index2];
         ar[index1] = indexs2;
         ar[index2] = indexs1;
     }
-    void shuffle(int* shufflearray, int n) {
+    void shuffle(vector<int>& shufflearray, int n) {
         srand(time(NULL));
         for (int i = 0; i < n; i++) {
             shufflearray[i] = i + 1;
@@ -18,28 +19,28 @@ public:
             swap(shufflearray, i, rand() % n);
         }
     }
-    void printarray(int* ar, int len) {
+    void printarray(vector<int> ar, int len) {
         for (int i = 0; i < len; i++) {
             std::cout << ar[i] << " : ";
         }
         std::cout << std::endl;
     }
-    void sort(int* ar, int len) {
+    void sort(vector<int>& ar, int len) {
         int correct = 0;
         while (correct != len) {
             for (int i = 0; i < len; i++) {
-                if ((ar[i] > ar[i + 1])) {
+                if ((ar[i] > ar[static_cast<std::vector<int, std::allocator<int>>::size_type>(i) + 1])) {
                     swap(ar, i, i + 1);
                 }
             }
             for (correct = 0; correct < len; correct++) {
-                if (ar[correct] > ar[correct + 1]) {
+                if (ar[correct] > ar[static_cast<std::vector<int, std::allocator<int>>::size_type>(correct) + 1]) {
                     break;
                 }
             }
         }
     }
-    bool check(int* ar, int len) {
+    bool check(vector<int>& ar, int len) {
         int checks = 0;
         for (checks; checks < len - 1; checks++) {
             if (ar[checks] > ar[checks + 1]) {
